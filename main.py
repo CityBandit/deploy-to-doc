787ca90161f164f99f32d0fd6ea967e19874422d
KariukiAntony <antonygichoya9@gmail.com>
2024-12-04 17:17:09 +0300

    another commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def route():
-    return jsonify({"message": "Hello world"}), 200
+    return jsonify({"message": "Hello world1"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
